7e10cb3903958e850e2647f19a27e886db1a02c9
MoniMaddy <monymathes@gmail.com>
2020-07-13 23:29:23 +0530

    Updates

---
+++ precache-manifest.1358505368b7072cef2ce437b5ecea16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f733860655b478602f071bd2ea1871b8",
+    "url": "/React-in-github-demo/index.html"
+  },
+  {
+    "revision": "a551f66c3f55a203ff8a",
+    "url": "/React-in-github-demo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "742a4f3a84c4a1728761",
+    "url": "/React-in-github-demo/static/css/main.df1bbefe.chunk.css"
+  },
+  {
+    "revision": "a551f66c3f55a203ff8a",
+    "url": "/React-in-github-demo/static/js/2.162c5c2f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-in-github-demo/static/js/2.162c5c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742a4f3a84c4a1728761",
+    "url": "/React-in-github-demo/static/js/main.40d07701.chunk.js"
+  },
+  {
+    "revision": "8c0035eb69216755e1b7",
+    "url": "/React-in-github-demo/static/js/runtime-main.522296ae.js"
+  },
+  {
+    "revision": "05ec05937b6f614a0876bcb8146a04e2",
+    "url": "/React-in-github-demo/static/media/img2.05ec0593.jpg"
+  }
+]);
