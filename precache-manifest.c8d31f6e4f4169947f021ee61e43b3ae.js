d76fe502fd0fd9c37ed373177c9ef36b8f674393
MoniMaddy <monymathes@gmail.com>
2020-07-13 23:30:33 +0530

    Updates

---
+++ precache-manifest.c8d31f6e4f4169947f021ee61e43b3ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34433ae5c9395cff0857d5f5aa747629",
+    "url": "/ShoppingCart/index.html"
+  },
+  {
+    "revision": "a551f66c3f55a203ff8a",
+    "url": "/ShoppingCart/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "50e40145be6b6af57e70",
+    "url": "/ShoppingCart/static/css/main.df1bbefe.chunk.css"
+  },
+  {
+    "revision": "a551f66c3f55a203ff8a",
+    "url": "/ShoppingCart/static/js/2.162c5c2f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ShoppingCart/static/js/2.162c5c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e40145be6b6af57e70",
+    "url": "/ShoppingCart/static/js/main.4bff44b1.chunk.js"
+  },
+  {
+    "revision": "cb5e59a00c46b0697ae7",
+    "url": "/ShoppingCart/static/js/runtime-main.dbe14015.js"
+  },
+  {
+    "revision": "05ec05937b6f614a0876bcb8146a04e2",
+    "url": "/ShoppingCart/static/media/img2.05ec0593.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-in-github-demo/precache-manifest.1358505368b7072cef2ce437b5ecea16.js"
+  "/ShoppingCart/precache-manifest.c8d31f6e4f4169947f021ee61e43b3ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-in-github-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShoppingCart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
